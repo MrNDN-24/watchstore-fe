2e81340a2de714a352abd52fc85766d525e30de1
Khoagec <violympichongkhoa@gmail.com>
2024-10-28 21:23:25 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignUpPage from "./pages/SignUpPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Home from "./pages/HomePage";
 
@@ -10,6 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Thêm route mặc định redirect đến trang login */}
+        <Route path="/" element={<Navigate to="/auth/login" replace />} />
+
         <Route path="/home" element={<Home />} />
         <Route path="/auth/register" element={<SignUpPage />} />
         <Route path="/auth/login" element={<LoginPage />} />
